23ac3159f9e75c3e8be01dbb49044ab27e92b74d
Anant raj <heyanantraj@gmail.com>
2021-12-19 14:51:34 -0600

    databaseConn

--- proto_believe/proto_believe/settings.py
+++ proto_believe/proto_believe/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'proto_believe.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'proto_believe',
+        'USER': 'postgres',
+        'PASSWORD': 'Welcome@2020',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
